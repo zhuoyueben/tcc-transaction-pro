03e24ab7d9511b8626376d917c46683e3730b5c6
changming.xie <changming.xie@192.168.0.104>
2017-04-07 22:55:09 +0800

    add http projects

---
+++ tcc-transaction-tutorial-sample/tcc-transaction-http-sample/tcc-transaction-http-order/src/main/java/org/mengyun/tcctransaction/sample/http/order/infrastructure/serviceproxy/CapitalTradeOrderServiceProxy.java
@@ -0,0 +1,14 @@
+package org.mengyun.tcctransaction.sample.http.order.infrastructure.serviceproxy;
+
+import org.mengyun.tcctransaction.sample.http.capital.api.CapitalTradeOrderService;
+import org.mengyun.tcctransaction.sample.http.capital.api.dto.CapitalTradeOrderDto;
+
+/**
+ * Created by changming.xie on 4/7/17.
+ */
+public class CapitalTradeOrderServiceProxy implements CapitalTradeOrderService {
+    @Override
+    public String record(CapitalTradeOrderDto tradeOrderDto) {
+        return null;
+    }
+}
